c12b4a28bb745017a67cffb1cdf4b3177e3a20e4
JPYK <jpyk@camelot-itlab.com>
2019-03-01 16:37:33 +0100

    Init

---
+++ lib/src/components/home/home_component.dart
@@ -0,0 +1,11 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  templateUrl: "home_component.html",
+  styleUrls: ["home_component.css"],
+  selector: "home"
+)
+
+class HomeComponent {
+
+}
