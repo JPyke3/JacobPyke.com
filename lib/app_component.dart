da65b65beb048d5e261e45cb171a8b7e11fc6418
JPYK <jpyk@camelot-itlab.com>
2019-03-01 19:42:28 +0100

    Initial Scaffolding

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,17 +1,30 @@
 import 'package:angular/angular.dart';
 import 'package:jacobpyke/src/components/home/home_component.dart';
-
-import 'src/todo_list/todo_list_component.dart';
-
+import 'package:angular_components/app_layout/material_persistent_drawer.dart';
+import 'package:angular_components/content/deferred_content.dart';
+import 'package:angular_components/material_button/material_button.dart';
+import 'package:angular_components/material_icon/material_icon.dart';
 // AngularDart info: https://webdev.dartlang.org/angular
 // Components info: https://webdev.dartlang.org/components
 
+enum PageSelection {home, resume, repos}
+
 @Component(
   selector: 'my-app',
-  styleUrls: ['app_component.css'],
+  styleUrls: const ['package:angular_components/app_layout/layout.scss.css', 'app_component.css'],
   templateUrl: 'app_component.html',
-  directives: [HomeComponent],
+  directives: [
+    DeferredContentDirective,
+    MaterialButtonComponent,
+    MaterialPersistentDrawerDirective,
+    MaterialIconComponent,
+    MaterialPersistentDrawerDirective,
+    NgIf,
+    HomeComponent
+  ],
+  exports: const [PageSelection]
 )
-class AppComponent {
-  // Nothing here yet. All logic is in TodoListComponent.
+class AppComponent { 
+  PageSelection currentpage = PageSelection.home;
+
 }
